17616a929f86c38a73d8dd683cc48e38dc07053c
Administrator <182316962@qq.com>
2018-05-15 19:53:07 +0800

    change

--- saga01/src/App.js
+++ saga01/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Header from "./components/Index/Header";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <Header/>
+        </div>
+        <div className="App-body">
+          <div className="welcome-view">
+            <div className="category-nav">
+              <div>1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd</div>
+            </div>
+            <div className="main">
+              21adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd
+            </div>
+            <div className="sidebar">
+              31adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
